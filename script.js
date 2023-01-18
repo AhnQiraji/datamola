994b9bc2483527ce8a9894f8e5d616a995a84438
AhnQiraji <noitora_shiffer@mail.ru>
2023-01-18 22:22:03 +0300

    Feat: understandable nav

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let currentPage = 'home';
+document.getElementById('home-link').style.filter = 'invert(1)';
 
 function swipe(newPage) {
     if (currentPage !== newPage) {
         document.getElementById(newPage).style.display = 'flex';
+        document.getElementById(newPage+'-link').style.filter = 'invert(1)';
         document.getElementById(currentPage).style.display = 'none';
+        document.getElementById(currentPage+'-link').style.filter = '';
         currentPage = newPage;
     }
 }
