66bf4cc74ad03f00246ad03a13a277e5c51403b7
AhnQiraji <noitora_shiffer@mail.ru>
2023-01-19 02:06:52 +0300

    Feat: more content, saving page

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-let currentPage = 'home';
-document.getElementById('home-link').style.filter = 'invert(1)';
+let currentPage = localStorage.getItem('lastSessionPage');
+
+if (currentPage !== 'home' && currentPage !== null) {
+    document.getElementById(currentPage).style.display = 'flex';
+    document.getElementById(currentPage+'-link').style.filter = 'invert(1)';
+    document.getElementById('home').style.display = 'none';
+    document.getElementById('home-link').style.filter = '';
+} else {
+    currentPage = 'home';
+    document.getElementById('home-link').style.filter = 'invert(1)';
+}
 
 function swipe(newPage) {
     if (currentPage !== newPage) {
@@ -8,5 +17,6 @@ function swipe(newPage) {
         document.getElementById(currentPage).style.display = 'none';
         document.getElementById(currentPage+'-link').style.filter = '';
         currentPage = newPage;
+        localStorage.lastSessionPage = currentPage;
     }
 }
